a6b1ca040396cff8a26e645b47185ebc6b07d1e5
Endorth <rogervc4@gmail.com>
2023-08-01 21:38:49 +0200

    final polish

--- scenes/Infinitica.gd
+++ scenes/Infinitica.gd
@@ -12,8 +12,7 @@ onready var record_user_lab = $RecordUserLab
 
 
 var player_list = []
-var valid_actions = ["!b ", "!buy ", 
-											"!s ", "!sell "]
+
 
 var is_game_start : bool = false
 
@@ -30,18 +29,13 @@ var main_numb_lab_pos : Vector2 = Vector2(396, 512)
 var shame_user_lab_pos : Vector2 = Vector2(144, 448)
 var shame_numb_lab_pos : Vector2 = Vector2(156, 512)
 
-class MyCustomSorter:
-	static func sort_descending(a, b):
-		if a[0] > b[0]:
-			return true
-		return false
 
 func _ready():
-
 	record_number_lab.text = str(record_number)
 	record_user_lab.text = record_user
 	curr_username_lab.rect_position = main_user_lab_pos
 	curr_usernum_lab.rect_position = main_numb_lab_pos
+	
 	TwitchChat.connect("new_message", self, "send_data")
 
 func send_data(data):
@@ -50,19 +44,10 @@ func send_data(data):
 		var msg = data["msg"]
 		if msg.begins_with("-"):
 			add_number(user, msg)
-	
-func enter_action(msg):
-	var r = randi()% 10
-	var user = 'endorth' + str(r)
-	
-	if msg.begins_with("-"):
-		add_number(user, msg)
-	$SendButton/LineEdit.clear()
 
 func add_number(plyr, msg):
 	var value : int = 0
 	value = int(msg)
-	
 	if plyr != last_user:
 		if is_correct_number(value):
 			update_number(plyr, value)
@@ -70,7 +55,6 @@ func add_number(plyr, msg):
 			restart_game(plyr)
 
 func update_number(plyr, value):
-#		curr_username_lab.grow_horizontal = 0
 	curr_username_lab.rect_position = main_user_lab_pos
 	curr_usernum_lab.rect_position = main_numb_lab_pos
 	last_number = value
@@ -94,7 +78,7 @@ func update_number(plyr, value):
 		
 		
 func restart_game(plyr):
-#	curr_username_lab.grow_horizontal = 2
+
 	curr_username_lab.rect_position = shame_user_lab_pos
 	curr_usernum_lab.rect_position = shame_numb_lab_pos
 	
@@ -114,9 +98,5 @@ func update_record(p, v):
 func is_correct_number(v) -> bool:
 	return (v == last_number - 1)
 
-func _on_ResetButton_pressed():
-	is_game_start = false
 
-func _on_SendButton_pressed():
-	enter_action($SendButton/LineEdit.text)
 
--- scenes/Menu.gd
+++ scenes/Menu.gd
@@ -5,4 +5,16 @@ extends Control
 func _on_ConnectButton_pressed():
 	TwitchChat.channel = $LineEdit.text
 	TwitchChat._anon_connection()
+
+
+
+func _on_ExitButton_pressed():
+	get_tree().quit()
+
+
+func _on_TextureButton_toggled(button_pressed):
+	OS.window_borderless = button_pressed
+
+
+func _on_StartButton_pressed():
 	get_tree().change_scene("res://scenes/Infinitica.tscn")
